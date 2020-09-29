fc19b0cddc940edc899df1b101ac11874159f1a5
danakim21 <danakim21@gmail.com>
2020-09-29 22:31:40 +0900

    Updates

---
+++ precache-manifest.4eb5f976786d8c0cafbcba9df82d63b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c441d3c1fb6cb31c5a0c820f69a490",
+    "url": "/pig-latin-translator-app/index.html"
+  },
+  {
+    "revision": "d0d0bb8b9255aaa799a5",
+    "url": "/pig-latin-translator-app/static/css/main.2e33a140.chunk.css"
+  },
+  {
+    "revision": "2794950592f22a2d3a26",
+    "url": "/pig-latin-translator-app/static/js/2.159c9a27.chunk.js"
+  },
+  {
+    "revision": "e2f87207c54a792fc865eb19d6b296e4",
+    "url": "/pig-latin-translator-app/static/js/2.159c9a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d0bb8b9255aaa799a5",
+    "url": "/pig-latin-translator-app/static/js/main.f59324ce.chunk.js"
+  },
+  {
+    "revision": "18964071491395e3ea8d",
+    "url": "/pig-latin-translator-app/static/js/runtime-main.44f0a68f.js"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/pig-latin-translator-app/static/media/Lato-Black.e631d273.ttf"
+  }
+]);
